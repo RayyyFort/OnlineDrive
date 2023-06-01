1e4d692a45a4c510104aad546b103adb692d3293
gaRRycaRRyer <garrycarryer@gmail.com>
2023-05-31 23:01:25 -0400

    Added random color for header

--- resources/js/Layouts/header.vue
+++ resources/js/Layouts/header.vue
@@ -1,5 +1,23 @@
+<script>
+    export default {
+        methods:{
+            randomcolor: function (){
+                return "#" + Math.floor(Math.random() * 16777215).toString(16)
+            }
+        },
+        computed:{
+            styleObject: function () {
+                return{
+                    'background-color':this.randomcolor(),
+                    color:this.randomcolor()
+                }
+            }
+        },
+    }
+</script>
+
 <template>
-    <header>
+    <header :style="styleObject">
         <div class="d-flex justify-content-around">
             <p>logo</p>
             <div>
