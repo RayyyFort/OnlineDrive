5c39017b14e3e24c9a2b6566f5d6faa5433cfad1
gaRRycaRRyer <garrycarryer@gmail.com>
2023-06-06 07:02:49 -0400

    Rearranged the Files page layout

--- resources/js/Pages/Files.vue
+++ resources/js/Pages/Files.vue
@@ -12,11 +12,55 @@
 </script>
 
 <template>
-  <div style="height: 100%; width: 100%;">
+  <div style="height: 100%; width: 100%; display: flex; flex-direction: column;">
     <MyHeader :logged="this.logged"/>
-    <main style="max-height: inherit; display: flex; flex-direction: column; height: 100%; border: 1px solid white;">
-      <div style="max-width: 80%; max-height: 80%; width:100%; height: 100%; margin: auto; border: 1px solid white;">
-        <p>test file</p>
+    <main style="display: flex; flex-direction: column; height: 100%; border: 1px solid white;">
+      <h1 style="margin: auto; margin-top: 10px; margin-bottom: 10px;">Your Files</h1>
+      <div style="max-width: 80%; max-height: 90%; width:100%; height: 100%; margin: auto; border: 1px solid black;">
+        <table>
+          <thead>
+            <th>
+              PATH TO THE CURRENT FOLDER
+            </th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <p>test file</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>test file</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>test file</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>test file</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>test file</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>test file</p>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <p>test file</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </main>
   </div>
