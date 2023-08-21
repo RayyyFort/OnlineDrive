8801bd4c2f16e3c0021b0540e7c6fa14a8c5dfab
gaRRycaRRyer <garrycarryer@gmail.com>
2023-08-21 14:21:50 -0400

    IDK

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,6 +30,12 @@ return [
 
     'disks' => [
 
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'throw' => false,
+        ],
+
         'temp' => [
             'driver' => 'local',
             'root' => storage_path('temp'),
